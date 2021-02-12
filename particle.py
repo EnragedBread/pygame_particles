bc9f35deeb7155a1d44d06bd5676dbc4a5587fdb
Bread <civisimulator@gmail.com>
2021-02-11 20:55:34 -0500

    add Particle.burned_out()

--- particle.py
+++ particle.py
@@ -4,40 +4,43 @@ import pygame
 
 class Particle():
     def __init__(self, placement):
-        self.placement = placement # [mx, my]
-        self.radius = random.randint(4,6)
-        self.velocity = [random.randint(0, 7) - 3.5, random.randint(0, 7) - 3.5]
+        self._placement = placement # [mx, my]
+        self._radius = random.randint(4,6)
+        self._velocity = [random.randint(0, 7) - 3.5, random.randint(0, 7) - 3.5]
+
+    def burned_out(self):
+        return self._radius <= 0
 
     def render(self, screen):
         pygame.draw.circle(
             screen,
             pygame.Color('white'),
-            (int(self.placement[0]), int(self.placement[1])),
-            int(self.radius)
+            (int(self._placement[0]), int(self._placement[1])),
+            int(self._radius)
         )
 
     def update(self):
-        self.placement[0] += self.velocity[0]
-        self.placement[1] += self.velocity[1]
+        self._placement[0] += self._velocity[0]
+        self._placement[1] += self._velocity[1]
 
-        self.velocity[1] += 0.1 #gravity
-        self.radius -= 0.1
+        self._velocity[1] += 0.1 #gravity
+        self._radius -= 0.1
 
 class Particles():
     def __init__(self):
-        self.particles = []
+        self._particles = []
 
     def spawn(self, location):
-        self.particles.append(Particle(location))
+        self._particles.append(Particle(location))
 
     def render(self, screen):
-        for particle in self.particles:
+        for particle in self._particles:
             particle.render(screen)
 
     def update(self):
         # work with the list in reverse order so we can safely remove items while iterating
-        for i, particle in sorted(enumerate(self.particles), reverse=True):
+        for i, particle in sorted(enumerate(self._particles), reverse=True):
             particle.update()
 
-            if particle.radius <= 0:
-                self.particles.pop(i)
+            if particle.burned_out():
+                self._particles.pop(i)
